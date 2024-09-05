90ece120a71e62e9f1fd461bea5eb283729088f1
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-09-05 21:53:03 +0100

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Header from './components/Header';
 
+import useState from 'react';
+
+const initialData = {
+  initialInvestment: 10000,
+  annualInvestment: 500,
+  expectedReturn: 100,
+  duration: 5
+}
+
 function App() {
+  const [ data, setData ] = useState(initialData);
+  
   return (
       <Header title={"Investment Calculator"} />
-      
+
     )
 }
 
