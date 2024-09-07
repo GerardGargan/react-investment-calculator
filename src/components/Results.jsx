368caff96325e1393df41dceb1ac25b8c778a783
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-09-07 11:50:40 +0100

    update table and number formatting

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,27 +1,30 @@
 import { calculateInvestmentResults, formatter } from '../util/investment';
 
 export default function Results({ data }) {
-    console.log(data);
     const result = calculateInvestmentResults(data);
-    console.log(result);
+    const initialInvestment = result[0].valueEndOfYear - result[0].interest - result[0].annualInvestment;
     return (
-        <table>
+        <table id="result">
             <thead>
                 
                 <th>Year</th>
-                <th>Interest</th>
-                <th>Value end of year</th>
-                <th>Duration</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
                 
             </thead>
             <tbody>
                 {result.map((x) => {
+                    const totalInterest = x.valueEndOfYear - x.annualInvestment * x.year - initialInvestment;
+                    const totalAmountInvested = x.valueEndOfYear - totalInterest;
                     return (<>
-                        <tr>
+                        <tr key={x.year}>
                             <td>{x.year}</td>
-                            <td>{x.interest}</td>
-                            <td>{x.valueEndOfYear}</td>
-                            <td>{x.annualInvestment}</td>
+                            <td>{formatter.format(x.valueEndOfYear)}</td>
+                            <td>{formatter.format(x.interest)}</td>
+                            <td>{formatter.format(totalInterest)}</td>
+                            <td>{formatter.format(totalAmountInvested)}</td>
                         </tr>
                         </>
                     );
